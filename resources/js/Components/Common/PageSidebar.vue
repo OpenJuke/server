3079612304de54bc2d28b1b1847864b70d4f6e82
Laura Heimann <hello@laura.media>
2021-11-28 13:53:43 +0100

    added sidebar style

--- resources/js/Components/Common/PageSidebar.vue
+++ resources/js/Components/Common/PageSidebar.vue
@@ -6,8 +6,14 @@
             </Link>
         </header>
         <nav>
-            <Link :href="$route('discovery.index')">DiscoveryIndex</Link>
-            <Link :href="$route('admin.users.index')">AdminUsersIndex</Link>
+            <Link :href="$route('discovery.index')" :class="{'active': currentUrl === 'discovery.index'}">
+                <span class="mdi mdi-view-dashboard"></span>
+                <div>Discovery</div>
+            </Link>
+            <Link :href="$route('admin.users.index')" :class="{'active': currentUrl === 'admin.users.index'}">
+                <span class="mdi mdi-account-group"></span>
+                <div>Users</div>
+            </Link>
         </nav>
     </aside>
 </template>
@@ -19,6 +25,14 @@ export default {
     name: 'PageSidebar',
     components: {
         Link
+    },
+    props: {
+        currentUrl: {
+            type: String,
+            default() {
+                return '';
+            }
+        }
     },
     computed: {
         user() {
@@ -38,6 +52,44 @@ export default {
 
             & img {
                 height: 35px;
+            }
+        }
+        & nav {
+            align-self: flex-start;
+            display: grid;
+            grid-gap: 5px;
+            align-items: flex-start;
+            width: calc(100% - 25px);
+
+            & a {
+                background: transparent;
+                display: grid;
+                grid-template-columns: 24px 1fr;
+                grid-gap: 10px;
+                padding: 10px 25px;
+                border-top-right-radius: 50px;
+                border-bottom-right-radius: 50px;
+                text-decoration: none;
+                align-items: center;
+                color: rgba(255,255,255,0.6);
+                transition: 0.2s ease-in-out all;
+
+                & span {
+                    font-size: 24px;
+                }
+                & div {
+                    font-size: 18px;
+                }
+
+                &:hover {
+                    background: rgba(255,255,255,0.07);
+                    cursor: pointer;
+                }
+
+                &.active {
+                    background: rgba(252,59,181,0.15);
+                    color: #FC3BB5;
+                } 
             }
         }
     }
